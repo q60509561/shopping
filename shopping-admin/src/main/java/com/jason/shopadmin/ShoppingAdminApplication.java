75e094db10d6069b81a30ee67d3d5bc8bc0c8684
JasonChen <72369929+q60509561@users.noreply.github.com>
2021-07-24 08:53:59 +0800

    init project

---
+++ shopping-admin/src/main/java/com/jason/shopadmin/ShoppingAdminApplication.java
@@ -0,0 +1,13 @@
+package com.jason.shopadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingAdminApplication.class, args);
+	}
+
+}
---
+++ shopping-admin/src/test/java/com/jason/shopadmin/ShoppingAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.shopadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
